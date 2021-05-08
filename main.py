bf0c1e49e895448b52e06a9553cce622f04ade59
yonatanyell <69857977+yonatanyell@users.noreply.github.com>
2021-05-08 15:20:05 +0300

    Function grader: implementing the grading algorithm

---
+++ main.py
@@ -0,0 +1,10 @@
+# This is a sample Python script.
+
+# Press ⌃R to execute it or replace it with your code.
+# Press Double ⇧ to search everywhere for classes, files, tool windows, actions, and settings.
+
+import Performance_class
+
+littleY = Performance_class.Performance('LittleY_mixed.aif.MID', "littleY", "Y", 'LittleYon_perfect.aif.MID')
+print(littleY.grader())
+
