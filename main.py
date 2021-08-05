a72673f1d748b943bad0d9d64d5915a4daaaac7e
GiladBaruchTAU <65009510+GiladBaruchTAU@users.noreply.github.com>
2021-08-05 15:36:43 +0300

    Ginali Tempo Feature

--- main.py
+++ main.py
@@ -2,12 +2,14 @@
 
 # Press ⌃R to execute it or replace it with your code.
 # Press Double ⇧ to search everywhere for classes, files, tool windows, actions, and settings.
+import time
 
 import Performance_class
 import auxiliary
 
 test_data = auxiliary.create_random_mistakes("musicsamples/91a_GinaLi.midi", "GinaLi", 100, min_noise=0,
-                                             max_noise=0.2, min_percentage=0.1, max_percentage=0.2)
+                                             max_noise=0.5, min_percentage=0.1, max_percentage=0.5)
 for i, data in enumerate(test_data):
     path = "musicsamples/GinaLi/test" + str(i) + ".mid"
     auxiliary.np2mid(data.midi_df, path)
+
