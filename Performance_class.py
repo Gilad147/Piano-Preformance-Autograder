ffab002461b7b5f36423a74bda3c466dca4852ca
yonatanyell <69857977+yonatanyell@users.noreply.github.com>
2021-09-06 13:41:07 +0300

    give_labels added

--- Performance_class.py
+++ Performance_class.py
@@ -1,18 +1,7 @@
-import pickle
-
 import pretty_midi
 import pandas as pd
-import libfmp.c1
 import numpy as np
-from matplotlib import pyplot as plt
 from difflib import SequenceMatcher
-import sklearn as skl
-
-next_step_model_path = 'finalized_next_step_model.pkl'
-pitch_model_path = 'pitch_model.pkl'
-rhythm_model_path = 'rhythm_model.pkl'
-a_d_model_path = 'a_d_model.pkl'
-tempo_model_path = 'tempo_model.pkl'
 
 
 class Performance:
@@ -55,18 +44,16 @@ class Performance:
                                      columns=['Start', 'End', 'Pitch', 'Velocity', 'Instrument']).to_numpy()
         self.original = np.sort(self.original, 0)
 
-    def predict_reccomendation(self, technical_grades, path=next_step_model_path):
-        with open(path, 'rb') as file:
-            model = pickle.load(file)
-        return model.predict(technical_grades)
+        self.teachers_grades = []  # [[Teacher's Pitch score, Teacher's Tempo score, Teacher's Rhythm score,
+                                    # Teacher's Articulation & Dynamics score, Teacher's next step]] (similar to the order in qualtrics)
+
+        self.labels = [] # [Pitch, Tempo, Rhythm, Articulation & Dynamics, Next step]
 
     def predict_grades(self, technical_grades):
-        pitch_feature = self.predict_reccomendation(technical_grades, pitch_model_path)
-        rhythm_feature = self.predict_reccomendation(technical_grades, rhythm_model_path)
-        a_d_feature = self.predict_reccomendation(technical_grades, a_d_model_path)
-        tempo_feature = self.predict_reccomendation(technical_grades, tempo_model_path)
+        return None
 
-        return rhythm_feature, a_d_feature, pitch_feature, tempo_feature
+    def predict_reccomendation(self, technical_grades):
+        return None
 
     def get_features(self):
         try:
@@ -87,18 +74,6 @@ class Performance:
             return rhythm_feature, velocity_feature, duration_feature, pitch_feature, tempo_feature
         except:
             return -1, 0, 0, 0, 0
-
-    def visualise(self):
-        score = []
-        for instrument in self.midi_data.instruments:
-            for note in instrument.notes:
-                start = note.start
-                duration = note.end - start
-                pitch = note.pitch
-                velocity = note.velocity / 128.
-                score.append([start, duration, pitch, velocity, instrument.name])
-        fig, ax = libfmp.c1.visualize_piano_roll(score, figsize=(8, 3), velocity_alpha=True)
-        plt.show()
 
     def mistakes_generator(self, feature, noise=0.75, percentage=1, original=True):
         if original:
@@ -140,95 +115,6 @@ class Performance:
                     note[2] = 1 + note[2]
         self.midi_df = new_midi_df
 
-    def classifier(self):
-
-        logisticRegr = skl.linear_model.LogisticRegression()
-
-    def baseline_grader(self, sigma=20):
-        """
-        :param sigma: sigma for error functions
-        :return: Dictionary with performance grades
-        """
-        orig = self.original
-        stud = self.midi_df
-        tempo_score = 0
-        a_d_score = 0
-        matching_notes = 0
-        orig_pitch_list = orig[:, 2]
-        stud_pitch_list = stud[:, 2]
-        # loop for checking blocks replayed
-        for i in range(3):
-            # returns list of triples describing matching subsequences
-            matcher = SequenceMatcher(a=orig_pitch_list, b=stud_pitch_list)
-            blocks = matcher.get_matching_blocks()
-            if len(blocks) == 1:
-                break
-            scores = self.blocks_grader(blocks, sigma)
-            if i == 0:
-                played_notes = scores[2]
-                blocks_count = len(blocks)
-            tempo_score += scores[0] * (1 - i * 0.25)
-            a_d_score += scores[1] * (1 - i * 0.25)
-            matching_notes += scores[2]
-            # if it is not the same song or the performance is messy don't iterate further
-            if played_notes / orig.shape[0] < 0.5:
-                break
-        # pitch grade is effected by number of initial blocks, missing and unnecessary notes
-        pitch_grade = played_notes / (orig.shape[0] * 2) + matching_notes / (stud.shape[0] * 2) - (
-                blocks_count - 2) / orig.shape[0]
-        pitch_grade = max(0, pitch_grade)
-        # to avoid "over fitting" of the blocks, the grade is normalized with respect to pitch grade
-        tempo_grade = (1 - tempo_score / matching_notes) * np.sqrt(pitch_grade)
-        a_d_grade = (1 - a_d_score / matching_notes) * np.sqrt(pitch_grade)
-        return {"pitch grade": pitch_grade, "tempo grade": tempo_grade, "articulation and dynamics grade": a_d_grade}
-
-    def blocks_grader(self, blocks, sigma):
-        """
-
-        :param blocks: a list of triples describing student's matching sub-sequences played
-        :param sigma: sigma for scoring functions
-        :return: tempo grade, a_d_grade - articulation and dynamics grade, number of notes from the blocks that the student played.
-        """
-        orig = self.original
-        stud = self.midi_df
-        tempo_score = 0
-        a_d_score = 0
-        matching_notes = 0
-        for block in blocks:
-            # end of blocks list
-            if block[2] == 0:
-                break
-            # ignore single notes
-            if block[2] == 1:
-                continue
-            matching_notes += block[2]
-
-            # match timing of the two matching parts
-            orig_start = block[0]
-            stud_start = block[1]
-            orig_set_time = orig[orig_start, 0]
-            stud_set_time = stud[stud_start, 0]
-            for i in range(block[2]):
-                # testing the block's grades of timing and velocity
-                cur_orig_note = orig[orig_start + i]
-                cur_stud_note = stud[stud_start + i]
-                # calculate relative timing for each note in block
-                cur_orig_note[0] = cur_orig_note[0] - orig_set_time
-                cur_orig_note[1] = cur_orig_note[1] - orig_set_time
-                cur_stud_note[0] = cur_stud_note[0] - stud_set_time
-                cur_stud_note[1] = cur_stud_note[1] - stud_set_time
-                # ignore note in further analysis
-                cur_stud_note[2] = 0
-                # calculate grades for difference in notes
-                tempo_note_score, a_d_note_score = self.timing_velocity_grader(cur_orig_note, cur_stud_note,
-                                                                               sigma=sigma)
-                tempo_score += tempo_note_score
-                a_d_score += a_d_note_score
-                # reset timing in original performance
-                cur_orig_note[0] = cur_orig_note[0] + orig_set_time
-                cur_orig_note[1] = cur_orig_note[1] + orig_set_time
-
-        return tempo_score, a_d_score, matching_notes
 
     def supervised_blocks_diff(self, blocks):
         """
@@ -284,30 +170,29 @@ class Performance:
 
         return rhythm_diff, velocity_diff, duration_diff, matching_notes
 
-    def timing_velocity_grader(self, orig_note, stud_note, sigma=10):
-        """
 
-        :param orig_note:
-        :param stud_note:
-        :param sigma:
-        :return: grades for differences between notes with the same pitch
-        """
+    def give_labels(self, majority_or_avg): # majority_or_avg == true --> majority ; majority_or_avg == false --> avg
+        pitch_scores = [teacher[0] for teacher in self.teachers_grades]
+        tempo_scores = [teacher[1] for teacher in self.teachers_grades]
+        rhythm_scores = [teacher[2] for teacher in self.teachers_grades]
+        a_d_scores = [teacher[3] for teacher in self.teachers_grades]
+        next_step = [teacher[4] for teacher in self.teachers_grades]
 
-        orig_duration = orig_note[1] - orig_note[0]
-        stud_duration = stud_note[1] - stud_note[0]
-        distance = np.abs(stud_note[0] - orig_note[0])
-        diff_duration = np.abs(orig_duration - stud_duration)
-        timing_grade = self.deviation_of_note(distance, sigma=sigma // 2)
-        duration_grade = self.deviation_of_note(diff_duration, sigma=sigma // 5)
-        orig_velocity = orig_note[3]
-        stud_velocity = stud_note[3]
-        diff_velocity = np.abs(orig_velocity - stud_velocity)
-        velocity_grade = self.deviation_of_note(diff_velocity, sigma=sigma, grade="velocity")
-        return timing_grade, (2 / 3) * velocity_grade + (1 / 3) * duration_grade
-
-    def deviation_of_note(self, distance, sigma=5, grade="timing"):
-        if grade == "timing":
-            gausian = lambda x: np.exp(-0.5 * (x / sigma) ** 2)
+        if majority_or_avg:
+            labels = [max(set(pitch_scores), key=pitch_scores.count),
+                      max(set(tempo_scores), key=tempo_scores.count),
+                      max(set(rhythm_scores), key=rhythm_scores.count),
+                      max(set(a_d_scores), key=a_d_scores.count),
+                      max(set(next_step), key=next_step.count)]
         else:
-            gausian = lambda x: np.exp(-0.5 * (np.max([(x - 5), 1]) / sigma) ** 2)
-        return 1 - np.round(gausian(distance), 3)
+            labels = [round((sum(pitch_scores)/len(pitch_scores))),
+                      round((sum(tempo_scores)/len(tempo_scores))),
+                      round((sum(rhythm_scores)/len(rhythm_scores))),
+                      round((sum(a_d_scores)/len(a_d_scores))),
+                      round((sum(next_step)/len(next_step)))]
+
+        self.labels = labels
+
+
+
+
