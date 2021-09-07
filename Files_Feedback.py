0980ce0928cd113b1e9cbabfae0c3994772e6745
orpeleg <orpeleg@mail.tau.ac.il>
2021-09-07 13:25:34 +0300

    ReadMidi.py split into new module: Files_Feedback.py

---
+++ Files_Feedback.py
@@ -0,0 +1,174 @@
+import bisect
+from datetime import datetime
+import os
+import numpy as np
+
+
+# functions for numeric grades to written feedback
+def determine_rhythm_feedback(scores, breakpoints=[0.4, 0.8],
+                              grades=None):
+    if grades is None:
+        grades = ['you do not pay enough attention to rhythm',
+                  'try to pay more attention to rhythmic transitions',
+                  'great sense of rhythm, keep up']
+    i = bisect.bisect(breakpoints, scores)
+    return grades[i]
+
+
+def determine_pitch_feedback(scores, breakpoints=[0.4, 0.8],
+                             grades=None):
+    if grades is None:
+        grades = ['you have a lot of missing or wrong notes',
+                  'try to be more accurate with note sequences',
+                  'great melody, keep up']
+    i = bisect.bisect(breakpoints, scores)
+    return grades[i]
+
+
+def determine_tempo_feedback(scores, breakpoints=[0.4, 0.8],
+                             grades=None):
+    if grades is None:
+        grades = ['your feel of tempo is not consistent',
+                  'try to play with a better feel the tempo',
+                  'very steady playing, keep up']
+    i = bisect.bisect(breakpoints, scores)
+    return grades[i]
+
+
+def determine_velocity_feedback(scores, breakpoints=[0.4, 0.8],
+                                grades=None):
+    if grades is None:
+        grades = ['you are not sensitive enough to notes volume',
+                  'try to be more sensitive to the dynamics',
+                  'good expressive ability, keep up']
+    i = bisect.bisect(breakpoints, scores)
+    return grades[i]
+
+
+def determine_overall_feedback(scores, breakpoints=[0.4, 0.7, 0.9],
+                               grades=None):
+    if grades is None:
+        grades = ['there is still some work to do',
+                  'you did a good job', 'you played very well'
+                                        'it was excellent playing']
+    i = bisect.bisect(breakpoints, scores)
+    return grades[i]
+
+
+def save_feedback_to_directory(path, grades, feedback, recommendation):
+    text_file = open(path, "w")
+    text_file.write("Numeric scores: "
+                    "\nPitch-" + str(grades[0]) +
+                    "\nTempo-" + str(grades[1]) +
+                    "\nRhythm-" + str(grades[2]) +
+                    "\nArticulation-" + str(grades[3]))
+    text_file.write("\n\nVerbal Feedback: \n" + feedback[73:])
+    text_file.write("Recommendation: " + recommendation)
+    text_file.close()
+
+
+def feedback_for_exit_application(grades, recommendation, feedback_path):
+    overall_feedback = determine_overall_feedback(np.average(np.array(grades)))
+    pitching_feedback = determine_pitch_feedback(grades[3])
+    tempo_feedback = determine_tempo_feedback(grades[4])
+    rhythm_feedback = determine_rhythm_feedback(grades[0])
+    velocity_feedback = determine_velocity_feedback(grades[1])
+    recommendation_dictionary = {'0': 'play slower', '1': 'play it again', '2': 'play faster',
+                                 '3': 'play a easier piece', '4': 'play another piece',
+                                 '5': 'play a harder piece'}
+    feedback_message = overall_feedback + '\n' \
+                       + 'Please pay attention to this technicals: ' + '\n' + '\n' \
+                       + 'Pitch: ' + pitching_feedback + '\n' + '\n' \
+                       + 'Tempo: ' + tempo_feedback + '\n' + '\n' \
+                       + 'Rhythm: ' + rhythm_feedback + '\n' + '\n' \
+                       + 'Articulation: ' + velocity_feedback + '\n' + '\n'
+    save_feedback_to_directory(feedback_path, grades, feedback_message, recommendation_dictionary[recommendation])
+    return feedback_message, recommendation_dictionary[recommendation]
+
+
+def reformat_file_by_type(file_name):
+    # getting rid of file suffix
+    if 'ly' in file_name:
+        return file_name[:-3]
+    if 'png' in file_name:
+        return file_name[:-4]
+    if 'midi' in file_name:
+        return file_name[:-5]
+
+def next_piece_by_level(level, song_name):
+    """Randomly draw a piece by the level given
+        level: string digit stating the desired songbook level
+        :returns:
+                next_chart = the absolute path for the next chart
+                next_midi  = the absolute path for the next midi
+                name = the name of the chosen song without suffix
+                next_level = the level of the chosen song
+
+    # ATTENTION: treats each song as having .mid, .png, .ly files
+    # Change is necessary to prevent bugging if other files are present
+    """
+    next_level = level
+    if level <= 0:
+        next_level = 0
+    if level >= 3:
+        next_level = 3
+    directories_by_levels = {0: 'initial exercises', 1: 'initial exercises2', 2: 'initial exercises3',
+                             3: 'hebrew Collection'}
+    root_path = os.path.dirname(
+        os.path.abspath('project directory/songs' + '/' + directories_by_levels[next_level]))
+    next_songbook_path = os.path.join(root_path, directories_by_levels[next_level])
+    files_in_songbook = os.listdir(next_songbook_path)
+    chosen_piece = np.random.randint(0, len(files_in_songbook) / 3)
+    name = reformat_file_by_type(files_in_songbook[chosen_piece])
+    while name == song_name:
+        chosen_piece = np.random.randint(0, len(files_in_songbook) / 3)
+        name = reformat_file_by_type(files_in_songbook[chosen_piece])
+    song_files_indexes = np.char.startswith(np.array(files_in_songbook), name)
+    song_files = np.array(files_in_songbook)[song_files_indexes]
+    next_chart = song_files[np.char.endswith(song_files, 'png')]
+    midi_index = 1 - np.logical_or(np.char.endswith(song_files, 'png'), np.char.endswith(song_files, 'ly'))
+    next_midi = song_files[midi_index == 1]
+    next_chart = os.path.join(next_songbook_path, next_chart[0])
+    next_midi = os.path.join(next_songbook_path, next_midi[0])
+    return next_chart, next_midi, name, next_level
+
+def next_action_by_recommendation(recommendation, chart_path, original_midi, song_name, song_level):
+    # interprets predicted recommendation for student into the next trial settings
+    if int(recommendation) < 3:
+        return chart_path, original_midi, song_name, song_level
+    else:
+        if int(recommendation) == 4:
+            return next_piece_by_level(song_level, song_name)
+        else:
+            if int(recommendation) == 3:
+                return next_piece_by_level(song_level - 1, song_name)
+            else:
+                return next_piece_by_level(song_level + 1, song_name)
+
+
+def directories(Data_Played, subject_id, song_name):
+    """Saves data played into personal student directory
+        Data_Played: the midi events table designed as desired
+        :returns:
+                midi_path_to_save = absolute path of personal student directory to save midi file played
+    """
+    root_path = os.path.dirname(os.path.abspath("Piano-Preformance-Auto")) + "/Students recordings"
+    date_directory = datetime.date(datetime.now())
+    complete_date_directory = os.path.join(root_path, str(date_directory))
+    if not os.path.exists(complete_date_directory):
+        os.mkdir(complete_date_directory)
+    directory_of_subject = subject_id
+    complete_subject_directory = os.path.join(complete_date_directory, directory_of_subject)
+    if not os.path.exists(complete_subject_directory):
+        os.mkdir(complete_subject_directory)
+    now_time = str(datetime.time(datetime.now()))
+    if len(now_time) == 15:
+        now_time = now_time[:-7]
+    else:
+        now_time = now_time[:-6]
+    completeName = os.path.join(complete_subject_directory, now_time + "-" + song_name + ".txt")
+    midi_path_to_save = os.path.join(complete_subject_directory, now_time + "-" + song_name + ".midi")
+    with open(completeName, 'w') as output:
+        for row in Data_Played:
+            output.write(str(row) + '\n')
+    return midi_path_to_save
--- ReadMidi.py
+++ ReadMidi.py
@@ -1,8 +1,5 @@
 import itertools
 import math
-import os
-from datetime import datetime
-import bisect
 import pyaudio
 import pygame
 import pygame.midi
@@ -11,11 +8,11 @@ from tkinter import *
 from tkinter import messagebox
 from PIL import ImageTk, Image
 import tkinter.font as tkFont
-import numpy as np
 from pygame.midi import midi_to_frequency
 from Performance_class import Performance
 from auxiliary import np2mid
 import pretty_midi
+from Files_Feedback import *
 
 
 def midi(chart_path, original_midi, subject_id, song_name, song_level):
@@ -53,86 +50,15 @@ def midi(chart_path, original_midi, subject_id, song_name, song_level):
                         i += 1
         return New_Input[1:].astype(float)
 
-    # functions for numeric grades to written feedback
-    def determine_rhythm_feedback(scores, breakpoints=[0.4, 0.8],
-                                  grades=None):
-        if grades is None:
-            grades = ['you do not pay enough attention to rhythm',
-                      'try to pay more attention to rhythmic transitions',
-                      'great sense of rhythm, keep up']
-        i = bisect.bisect(breakpoints, scores)
-        return grades[i]
-
-    def determine_pitch_feedback(scores, breakpoints=[0.4, 0.8],
-                                 grades=None):
-        if grades is None:
-            grades = ['you have a lot of missing or wrong notes',
-                      'try to be more accurate with note sequences',
-                      'great melody, keep up']
-        i = bisect.bisect(breakpoints, scores)
-        return grades[i]
-
-    def determine_tempo_feedback(scores, breakpoints=[0.4, 0.8],
-                                 grades=None):
-        if grades is None:
-            grades = ['your feel of tempo is not consistent',
-                      'try to play with a better feel the tempo',
-                      'very steady playing, keep up']
-        i = bisect.bisect(breakpoints, scores)
-        return grades[i]
-
-    def determine_velocity_feedback(scores, breakpoints=[0.4, 0.8],
-                                    grades=None):
-        if grades is None:
-            grades = ['you are not sensitive enough to notes volume',
-                      'try to be more sensitive to the dynamics',
-                      'good expressive ability, keep up']
-        i = bisect.bisect(breakpoints, scores)
-        return grades[i]
-
-    def determine_overall_feedback(scores, breakpoints=[0.4, 0.7, 0.9],
-                                   grades=None):
-        if grades is None:
-            grades = ['there is still some work to do',
-                      'you did a good job', 'you played very well'
-                                            'it was excellent playing']
-        i = bisect.bisect(breakpoints, scores)
-        return grades[i]
-
-    def save_feedback_to_directory(path, grades, feedback, recommendation):
-        text_file = open(path, "w")
-        text_file.write("Numeric scores: "
-                        "\nPitch-" + str(grades[0]) +
-                        "\nTempo-" + str(grades[1]) +
-                        "\nRhythm-" + str(grades[2]) +
-                        "\nArticulation-" + str(grades[3]))
-        text_file.write("\n\nVerbal Feedback: \n" + feedback[73:])
-        text_file.write("Recommendation: " + recommendation)
-        text_file.close()
-
     def exit_application(grades, recommendation, feedback_path):
         """Handle end of trial and next step determination and set up
             grades: vector of size 4 with the predicted grades from ML processing
             recommendation: a string digit stating the predicted recommendation from ML processing
         """
-        overall_feedback = determine_overall_feedback(np.average(np.array(grades)))
-        pitching_feedback = determine_pitch_feedback(grades[3])
-        tempo_feedback = determine_tempo_feedback(grades[4])
-        rhythm_feedback = determine_rhythm_feedback(grades[0])
-        velocity_feedback = determine_velocity_feedback(grades[1])
-        recommendation_dictionary = {'0': 'play slower', '1': 'play it again', '2': 'play faster',
-                                     '3': 'play a easier piece', '4': 'play another piece',
-                                     '5': 'play a harder piece'}
-        feedback_message = overall_feedback + '\n' \
-                           + 'Please pay attention to this technicals: ' + '\n' + '\n' \
-                           + 'Pitch: ' + pitching_feedback + '\n' + '\n' \
-                           + 'Tempo: ' + tempo_feedback + '\n' + '\n' \
-                           + 'Rhythm: ' + rhythm_feedback + '\n' + '\n' \
-                           + 'Articulation: ' + velocity_feedback + '\n' + '\n'
-        save_feedback_to_directory(feedback_path, grades, feedback_message, recommendation_dictionary[recommendation])
+        feedback_message, verbal_recommendation = feedback_for_exit_application(grades, recommendation, feedback_path)
         MsgBox = messagebox.askquestion('End of Trial', feedback_message + '\n' +
                                         'I advice you to '
-                                        + recommendation_dictionary[recommendation] + '\n'
+                                        + verbal_recommendation + '\n'
                                                                                       'do you want to keep training?',
                                         icon='warning')
         keyboard.close()
@@ -140,92 +66,6 @@ def midi(chart_path, original_midi, subject_id, song_name, song_level):
         pygame.quit()
         if MsgBox == 'no':
             return True
-
-    def reformat_file_by_type(file_name):
-        # getting rid of file suffix
-        if 'ly' in file_name:
-            return file_name[:-3]
-        if 'png' in file_name:
-            return file_name[:-4]
-        if 'midi' in file_name:
-            return file_name[:-5]
-
-    def next_piece_by_level(level):
-        """Randomly draw a piece by the level given
-            level: string digit stating the desired songbook level
-            :returns:
-                    next_chart = the absolute path for the next chart
-                    next_midi  = the absolute path for the next midi
-                    name = the name of the chosen song without suffix
-                    next_level = the level of the chosen song
-
-        # ATTENTION: treats each song as having .mid, .png, .ly files
-        # Change is necessary to prevent bugging if other files are present
-        """
-        next_level = level
-        if level <= 0:
-            next_level = 0
-        if level >= 3:
-            next_level = 3
-        directories_by_levels = {0: 'initial exercises', 1: 'initial exercises2', 2: 'initial exercises3',
-                                 3: 'hebrew Collection'}
-        root_path = os.path.dirname(
-            os.path.abspath('project directory/songs' + '/' + directories_by_levels[next_level]))
-        next_songbook_path = os.path.join(root_path, directories_by_levels[next_level])
-        files_in_songbook = os.listdir(next_songbook_path)
-        chosen_piece = np.random.randint(0, len(files_in_songbook) / 3)
-        name = reformat_file_by_type(files_in_songbook[chosen_piece])
-        while name == song_name:
-            chosen_piece = np.random.randint(0, len(files_in_songbook) / 3)
-            name = reformat_file_by_type(files_in_songbook[chosen_piece])
-        song_files_indexes = np.char.startswith(np.array(files_in_songbook), name)
-        song_files = np.array(files_in_songbook)[song_files_indexes]
-        next_chart = song_files[np.char.endswith(song_files, 'png')]
-        midi_index = 1 - np.logical_or(np.char.endswith(song_files, 'png'), np.char.endswith(song_files, 'ly'))
-        next_midi = song_files[midi_index == 1]
-        next_chart = os.path.join(next_songbook_path, next_chart[0])
-        next_midi = os.path.join(next_songbook_path, next_midi[0])
-        return next_chart, next_midi, name, next_level
-
-    def next_action_by_recommendation(recommendation):
-        # interprets predicted recommendation for student into the next trial settings
-        if int(recommendation) < 3:
-            return chart_path, original_midi, song_name, song_level
-        else:
-            if int(recommendation) == 4:
-                return next_piece_by_level(song_level)
-            else:
-                if int(recommendation) == 3:
-                    return next_piece_by_level(song_level - 1)
-                else:
-                    return next_piece_by_level(song_level + 1)
-
-    def directories(Data_Played):
-        """Saves data played into personal student directory
-            Data_Played: the midi events table designed as desired
-            :returns:
-                    midi_path_to_save = absolute path of personal student directory to save midi file played
-        """
-        root_path = os.path.dirname(os.path.abspath("Piano-Preformance-Auto")) + "/Students recordings"
-        date_directory = datetime.date(datetime.now())
-        complete_date_directory = os.path.join(root_path, str(date_directory))
-        if not os.path.exists(complete_date_directory):
-            os.mkdir(complete_date_directory)
-        directory_of_subject = subject_id
-        complete_subject_directory = os.path.join(complete_date_directory, directory_of_subject)
-        if not os.path.exists(complete_subject_directory):
-            os.mkdir(complete_subject_directory)
-        now_time = str(datetime.time(datetime.now()))
-        if len(now_time) == 15:
-            now_time = now_time[:-7]
-        else:
-            now_time = now_time[:-6]
-        completeName = os.path.join(complete_subject_directory, now_time + "-" + song_name + ".txt")
-        midi_path_to_save = os.path.join(complete_subject_directory, now_time + "-" + song_name + ".midi")
-        with open(completeName, 'w') as output:
-            for row in Data_Played:
-                output.write(str(row) + '\n')
-        return midi_path_to_save
 
     def stop(recording):
         # process stopping button instance
@@ -244,7 +84,7 @@ def midi(chart_path, original_midi, subject_id, song_name, song_level):
         Data_Played = input_design(Raw_Input[1:].astype('float32'))
         Data_Played[:, 0] = Data_Played[:, 0] / 1000
         Data_Played[:, 1] = Data_Played[:, 1] / 1000
-        midi_path_to_save = directories(Data_Played)
+        midi_path_to_save = directories(Data_Played, subject_id, song_name)
         np2mid(Data_Played, midi_path_to_save, pretty_midi.PrettyMIDI(original_midi), True)
         performance = Performance(midi_path_to_save, song_name, subject_id, original_midi,
                                   prettyMidiFile_performance=None, prettyMidiFile_original=None)
@@ -269,7 +109,7 @@ def midi(chart_path, original_midi, subject_id, song_name, song_level):
                                                 '*choosing "no" means playing the same piece again', icon='warning')
                 if MsgBox == 'yes':
                     next_chart_path, next_original_midi, next_song_name, next_song_level = \
-                        next_action_by_recommendation(recommendation)
+                        next_action_by_recommendation(recommendation, chart_path, original_midi, song_name, song_level)
             window.destroy()
             next_chart_path = chart_path
             next_original_midi = original_midi
@@ -305,6 +145,7 @@ def midi(chart_path, original_midi, subject_id, song_name, song_level):
         record_button = Button(window, text='Record', image=photo2, command=lambda: record(Raw_Input))
         record_button.image = photo2
         record_button.place(x=140, y=80)
+        return record_button
 
     def place_clear_button():
         root_path = os.path.dirname(os.path.abspath("Piano-Preformance-Auto"))
@@ -313,9 +154,10 @@ def midi(chart_path, original_midi, subject_id, song_name, song_level):
         zoom = 0.2
         pixels_x, pixels_y = tuple([int(zoom * x) for x in Image.open(complete_path).size])
         photo2 = ImageTk.PhotoImage(Image.open(path).resize((pixels_x, pixels_y)))
-        record_button = Button(window, text='Clear', image=photo2, command=lambda: clear(Raw_Input))
-        record_button.image = photo2
-        record_button.place(x=280, y=80)
+        clear_button = Button(window, text='Clear', image=photo2, command=lambda: clear(Raw_Input))
+        clear_button.image = photo2
+        clear_button.place(x=280, y=80)
+        return clear_button
 
     def place_note_chart(path):
         zoom = 1.8
@@ -368,8 +210,7 @@ def midi(chart_path, original_midi, subject_id, song_name, song_level):
                                   "\nTo save recording and end trial press STOP", font=fontStyle)
     myLabel1.place(x=0, y=0)
     place_stop_button()
-    place_record_button()
-    place_clear_button()
+    record_or_reset_button = place_record_button()
     place_note_chart(chart_path)
     # Initialize MIDI
     pygame.init()
@@ -395,12 +236,16 @@ def midi(chart_path, original_midi, subject_id, song_name, song_level):
             except:
                 break
             if np.all(Raw_Input[0] == [1, 1, 1, 1]):
+                record_or_reset_button.destroy()
+                record_or_reset_button = place_clear_button()
                 reset_recording = True
                 Raw_Input = np.zeros((1, 4))
                 Recording_Pushed_Last = True
                 record_clear_state.destroy()
                 record_clear_state = place_recording_clearing_state(True)
             if np.all(Raw_Input[0] == [2, 2, 2, 2]):
+                record_or_reset_button.destroy()
+                record_or_reset_button = place_record_button()
                 reset_recording = True
                 Raw_Input = np.zeros((1, 4))
                 record_clear_state.destroy()
